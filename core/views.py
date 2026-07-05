9e32ff5ee614029e43c843435e5c790c09084f96
PrajnaPradhan07 <prajna.mca98@gmail.com>
2026-07-05 23:21:49 +0530

    add register view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from .models import User,Category,MenuItem,Order,OrderItem
+from .serializers import UserSerializer,RegisterSerializer,CategorySerializer,MenuItemSerializer,OrderSerializer,OrderItemSerializer
 
-# Create your views here.
+#Register View
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                'message': 'User registered successfully!',
+                'user': UserSerializer(user).data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
