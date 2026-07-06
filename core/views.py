d654c8ac9044da20b246b48114d1f9524078da01
PrajnaPradhan07 <prajna.mca98@gmail.com>
2026-07-06 23:06:08 +0530

    add Login View

--- core/views.py
+++ core/views.py
@@ -22,6 +22,25 @@ class RegisterView(APIView):
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+#Login View
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username = username,password = password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'message':'Login successful!',
+                'access':str(refresh.access_token),
+                'refresh':str(refresh),
+                'user': UserSerializer(user).data
+            },status = status.HTTP_200_OK)
+        return Response({'error':'Invalid credentials'},status = status.HTTP_401_UNAUTHORIZED)
+
+
 
 
 
