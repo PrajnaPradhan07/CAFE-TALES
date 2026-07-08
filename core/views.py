ef72523b7364115652d7b934213f76ed5ff32d4d
PrajnaPradhan07 <prajna.mca98@gmail.com>
2026-07-08 20:49:12 +0530

    add order and order detail view

--- core/views.py
+++ core/views.py
@@ -58,6 +58,60 @@ class MenuItemView(APIView):
         serializer = MenuItemSerializer(items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+#Order View
+class OrderView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        orders = Order.objects.filter(user=request.user)
+        serializer = OrderSerializer(orders, many =True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self,request):
+        items_data = request.data.get('items',[])
+        if not items_data :
+            return Response({'error':'No items provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+        order = Order.objects.create(user=request.user)
+        total = 0
+
+        for item_data in items_data:
+            menu_item = MenuItem.objects.get(id=item_data['menu_item'])
+            quantity = item_data.get('quantity',1)
+            price = menu_item.price * quantity
+            total += price
+            OrderItem.objects.create(
+                order=order,
+                menu_item = menu_item,
+                quantity = quantity,
+                price = price
+            )
+
+        order.total_price =total
+        order.save()
+
+        #add loyalty points (1 point for 10 rupees)
+        request.user.loyalty_points += int(total//10)
+        request.user.save()
+
+        serializer = OrderSerializer(order)
+        return Response({
+            'message':'Order placed successfully',
+            'order':serializer.data
+        }, status = status.HTTP_201_CREATED)
+
+#Order Detail View
+class OrderDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request,pk):
+        try:
+            order = Order.objects.get(id=pk,user =request.user)
+            serializer = OrderSerializer(order)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        except Order.DoesNotExist:
+            return Response({'error':'Order not found'},status=status.HTTP_404_NOT_FOUND)
+
 
 
 
